96d7f98cb8d2f12e37c24f2ca303fc24634646ea
Wojtek_Lap <wkx100@gmail.com>
2018-09-18 23:58:11 +0200

    Step 1

--- FizzBuzz/FizzBuzz.BLL/FizzBuzz/Services/FizzBuzzService.cs
+++ FizzBuzz/FizzBuzz.BLL/FizzBuzz/Services/FizzBuzzService.cs
@@ -10,12 +10,35 @@ namespace FizzBuzz.BLL.FizzBuzz.Services
     {
         public void DoFizzBuzz(string value)
         {
-            
+            var numericValue = int.Parse(value);
+
+            for (var i = 1; i < numericValue; i++)
+            {
+                var isDivisibleByThree = IsDivisible(i, 3);
+                var isDivisibleByFive = IsDivisible(i, 5);
+
+                if (isDivisibleByThree && isDivisibleByFive)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (isDivisibleByThree)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (isDivisibleByFive)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
 
         private static bool IsDivisible(int generatedValue, int divider)
         {
-            return true;
+            return generatedValue % divider == 0;
         }
     }
 }
--- FizzBuzz/FizzBuzz.Console/Program.cs
+++ FizzBuzz/FizzBuzz.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FizzBuzz.BLL.FizzBuzz.Services;
 
 namespace FizzBuzz.Console
 {
@@ -10,6 +11,11 @@ namespace FizzBuzz.Console
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Please enter a value between 1 and 1000");
+            var enterValue = System.Console.ReadLine();
+            var fizzBuzz = new FizzBuzzService();
+            fizzBuzz.DoFizzBuzz(enterValue);
+            System.Console.ReadKey();
         }
     }
 }
