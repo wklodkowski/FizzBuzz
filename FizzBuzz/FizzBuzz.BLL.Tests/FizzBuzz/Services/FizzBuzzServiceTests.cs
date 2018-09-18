e9aa1b4ef165d6be2207edeba6138ee8867ea51b
Wojtek_Lap <wkx100@gmail.com>
2018-09-18 23:52:04 +0200

    Add tests for FizzBuzzService

---
+++ FizzBuzz/FizzBuzz.BLL.Tests/FizzBuzz/Services/FizzBuzzServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using FizzBuzz.BLL.FizzBuzz.Services;
+using NUnit.Framework;
+
+namespace FizzBuzz.BLL.Tests.FizzBuzz.Services
+{
+    [TestFixture]
+    public class FizzBuzzServiceTests
+    {
+        [Test]
+        public void DoFizzBuzz_DivideByThree_PrintFizz()
+        {
+            var fizzBuzzService = new FizzBuzzService();
+            
+        }
+
+        [Test]
+        public void DoFizzBuzz_DivideByThree_PrintBuzz()
+        {
+            var fizzBuzzService = new FizzBuzzService();
+
+        }
+
+        [Test]
+        public void DoFizzBuzz_DivideByThreeAndFive_PrintFizzBuzz()
+        {
+            var fizzBuzzService = new FizzBuzzService();
+
+        }
+
+        [Test]
+        public void DoFizzBuzz_NotDivideByThreeOrFive_PrintNumber()
+        {
+            var fizzBuzzService = new FizzBuzzService();
+
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz.BLL/FizzBuzz/Services/FizzBuzzService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.BLL.FizzBuzz.Services
+{
+    public class FizzBuzzService
+    {
+        public void DoFizzBuzz(string value)
+        {
+            
+        }
+
+        private static bool IsDivisible(int generatedValue, int divider)
+        {
+            return true;
+        }
+    }
+}
